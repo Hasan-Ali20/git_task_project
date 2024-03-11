c7e634a214bcd93322d904acd32fb6418fc3644f
Hasan Ali <Hasan.ali487@gmail.com>
2024-03-11 00:24:49 +0000

    Used data type casting and then printed a message using an f string

--- details.py
+++ details.py
@@ -3,8 +3,9 @@
 #After this information is gathered, it will be then merged into one sentence.
 #To do this we will print the sentence as a string data type using all the different variables. 
 
-name = input ("what is your name?")
-age = input ("what is your age?")
-house_number = input ("What is your house number?")
-street_name = input ("What is your street name?")
-print("This is " + name + ". He is " + age + " years old and lives at house number " + house_number + " on " + street_name + ".")
+name = str(input("what is your name? "))
+age = int(input("what is your age? "))
+house_number = int(input("What is your house number? "))
+street_name = str(input("What is your street name? "))
+
+print(f"This is {name}. He is {age} years old and lives at house number {house_number} on {street_name}.")
